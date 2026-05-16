9340a5f779918a354ed1ce64c84ee2cd499875eb
Beltrano de Carvalho <victorhugosparapam@gmail.com>
2026-05-16 10:12:37 -0300

    ficha_CY2

---
+++ js.html/codigo.js
@@ -0,0 +1,13 @@
+// declaração
+var x;
+var y;
+var z;
+//multiplas declarações com um única "var"
+var a, b, c;
+// declaração e inicialização (atribuição)
+var alfa = 0, beta = 100;
+// var null
+var idade = null;
+var maisVelho = 3 * idade;//maisVelho vai ter valor igual a zero
+
+console.log(maisVelho);
